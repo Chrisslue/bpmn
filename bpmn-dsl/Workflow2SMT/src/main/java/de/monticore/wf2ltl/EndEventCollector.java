d6ad53040d3fcc0950b3b11286f073c75aca47b5
Jakob Krude <jakob.krude@rwth-aachen.de>
2023-05-09 19:49:08 +0200

    Collector using the Visitor pattern. Collect Named and InlineEvents in contrast to WorkflowCollectors.

---
+++ bpmn-dsl/Workflow2SMT/src/main/java/de/monticore/wf2ltl/EndEventCollector.java
@@ -0,0 +1,41 @@
+package de.monticore.wf2ltl;
+
+import de.monticore.bpmn.workflow._ast.ASTEvent;
+import de.monticore.bpmn.workflow._ast.ASTFlowElement;
+import de.monticore.bpmn.workflow._ast.ASTInlineEvent;
+import de.monticore.bpmn.workflow._ast.ASTNamedEvent;
+import de.monticore.bpmn.workflow._visitor.WorkflowTraverser;
+import de.monticore.bpmn.workflow._visitor.WorkflowTraverserImplementation;
+
+import java.util.List;
+
+public class EndEventCollector extends EventCollector {
+
+  public EndEventCollector(WorkflowTraverser traverser) {
+    super(traverser);
+  }
+
+  public static List<ASTEvent> of(List<ASTFlowElement> flowElements) {
+    var traverser = new WorkflowTraverserImplementation();
+    var collector = new EndEventCollector(traverser);
+    for (ASTFlowElement flowElement : flowElements) {
+      flowElement.accept(traverser);
+    }
+    return collector.getEvents();
+  }
+
+  @Override
+  public void handle(ASTNamedEvent namedEvent) {
+    if (namedEvent.isEnd()) {
+      super.getEvents().add(namedEvent);
+    }
+  }
+
+  @Override
+  public void handle(ASTInlineEvent node) {
+    if (node.isEnd()) {
+      super.getEvents().add(node);
+    }
+  }
+
+}
