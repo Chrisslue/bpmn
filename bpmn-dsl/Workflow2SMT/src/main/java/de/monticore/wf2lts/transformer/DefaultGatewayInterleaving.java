72ac08d72a747721a3f6765c303b5ee47993b47a
Jakob Krude <jakob.krude@rwth-aachen.de>
2023-06-15 13:38:58 +0200

    Add default implementation of GatewayInterleaving

---
+++ bpmn-dsl/Workflow2SMT/src/main/java/de/monticore/wf2lts/transformer/DefaultGatewayInterleaving.java
@@ -0,0 +1,37 @@
+package de.monticore.wf2ltl.transformer;
+
+import de.monticore.wf2ltl.datastructure.LTS;
+
+public class DefaultGatewayInterleaving implements GatewayInterleavingStrategy {
+
+  @Override
+  public LTS interleaveExclusive(LTS lts) {
+    return lts; // Noting to do
+  }
+
+  @Override
+  public LTS interleaveParallel(LTS lts) {
+    return DefaultParallelInterleaving.interleave(lts);
+  }
+
+  @Override
+  public LTS interleaveInclusive(LTS lts) {
+    return DefaultSequentialInterleaving.interleave(lts);
+  }
+
+  @Override
+  public LTS interleaveEventBasedParallel(LTS lts) {
+    return DefaultParallelInterleaving.interleave(lts);
+  }
+
+  @Override
+  public LTS interleaveEventBasedExclusive(LTS lts) {
+    return lts; // Nothing to do.
+  }
+
+  @Override
+  public LTS interleaveComplex(LTS lts) {
+    throw new UnsupportedOperationException("interleaveComplex is not yet implemented");
+  }
+
+}
