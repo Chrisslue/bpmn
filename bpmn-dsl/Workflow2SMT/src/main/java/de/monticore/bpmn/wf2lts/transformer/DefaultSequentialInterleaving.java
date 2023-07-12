efc87771ee8f40bebef36dabf1576d52e2fa1fff
Jakob Krude <jakob.krude@rwth-aachen.de>
2023-07-12 15:27:20 +0200

    [WIP] Fix SequentialInterleaving with final states

--- bpmn-dsl/Workflow2SMT/src/main/java/de/monticore/bpmn/wf2lts/transformer/DefaultSequentialInterleaving.java
+++ bpmn-dsl/Workflow2SMT/src/main/java/de/monticore/bpmn/wf2lts/transformer/DefaultSequentialInterleaving.java
@@ -1,107 +1,105 @@
 package de.monticore.bpmn.wf2lts.transformer;
 
-import de.monticore.bpmn.wf2lts.datastructure.LTS;
 import de.monticore.bpmn.wf2lts.datastructure.LTS.State;
 import de.monticore.bpmn.wf2lts.datastructure.LTS.Transition;
 import de.monticore.bpmn.wf2lts.datastructure.LTSTraverser;
+import de.monticore.bpmn.wf2lts.datastructure.LTSWithFinalStates;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Interleaving strategy designed for inclusive gateways. At an inclusive split-gateway in BPMN
- * every number of outgoing transitions can be taken. This strategy implements this in a sequential
- * way, such that one path has to be finished before the next begins. For example: s0 --> s1: A s0
- * --> s2: B s1 --> s3: C s2 --> s4: D Will become: s0 --> s1: A s1 --> s3: C s3 --> s5: B s5 -->
- * s6: D s0 --> s2: B s2 --> s4: D s3 --> s7: A s7 --> s8: B Even tho the strategy is especially
- * suited to be used for inclusive-gateways it could be also used for parallel gateways if desired.
+ * Interleaving strategy designed for inclusive gateways. At an inclusive split-gateway in BPMN every number of outgoing
+ * transitions can be taken. This strategy implements this in a sequential way, such that one path has to be finished
+ * before the next begins. For example: s0 --> s1: A s0 --> s2: B s1 --> s3: C s2 --> s4: D Will become: s0 --> s1: A s1
+ * --> s3: C s3 --> s5: B s5 --> s6: D s0 --> s2: B s2 --> s4: D s3 --> s7: A s7 --> s8: B Even tho the strategy is
+ * especially suited to be used for inclusive-gateways it could be also used for parallel gateways if desired.
  */
 public class DefaultSequentialInterleaving {
 
-  private final LTS oldLTS;
+  private final LTSWithFinalStates oldLTS;
 
-  private final LTS lts;
+  private final LTSWithFinalStates lts;
 
-  public DefaultSequentialInterleaving(LTS oldLTS) {
+  public DefaultSequentialInterleaving(LTSWithFinalStates oldLTS) {
     this.oldLTS = oldLTS;
-    this.lts = new LTS(oldLTS.getStart());
+    this.lts = new LTSWithFinalStates(oldLTS.getStart());
     this.lts.addLTS(oldLTS);
   }
 
-  public static LTS interleave(LTS lts) {
+  public static LTSWithFinalStates interleave(LTSWithFinalStates lts) {
     return new DefaultSequentialInterleaving(lts).interleave();
   }
 
-  private LTS interleave() {
+  private LTSWithFinalStates interleave() {
     // TODO What about transitions back to oldLTS.getStart()?
-    // TODO States that only have back-links should be considered terminal too?
     var startTransitions = oldLTS.getOutgoings(oldLTS.getStart());
 
-    List<LTS> subLTSCopies =
+    // Get the part of the old lts induced by the initial transition from start.
+    List<LTSWithFinalStates> subLTSCopies =
         startTransitions.stream()
             .map(transition -> subLTS(oldLTS, transition))
             .collect(Collectors.toList());
 
     for (int i = 0; i < startTransitions.size(); i++) {
-      var transition = startTransitions.get(i);
       var otherSubLTS = new ArrayList<>(subLTSCopies);
       otherSubLTS.remove(i);
-      for (var terminal : reachableTerminalStates(oldLTS, transition.getTarget())) {
-        recursiveAppend(terminal, otherSubLTS);
+      // Add the other sub-lts to each final state of the current sub-lts
+      for (var finalState : subLTSCopies.get(i).getFinalStates()) {
+        recursiveAppend(finalState, otherSubLTS);
       }
     }
     return lts;
   }
 
   /*
-   * Append every lts as possible continuation to the terminal state.
-   * If more than one element is in the list recursively add the remaining options to the terminal states of the added lts.
+   * Append every lts as possible continuation to the final state.
+   * If more than one element is in the list recursively add the remaining options to the final states of the added lts.
    */
-  private void recursiveAppend(State terminal, List<LTS> remainingLTS) {
-    for (LTS subLTS : remainingLTS) {
-      var copy = new LTS(subLTS);
-      addLTSToTerminal(terminal, copy);
-      for (var subLTSTerminal : copy.getTerminalStates()) {
+  private void recursiveAppend(State finalState, List<LTSWithFinalStates> remainingLTS) {
+    for (LTSWithFinalStates subLTS : remainingLTS) {
+      var deepCopy = new LTSWithFinalStates(subLTS);
+      appendLTSToFinalState(finalState, deepCopy);
+      for (var subLTSFinalState : deepCopy.getFinalStates()) {
         var nextRemaining = new ArrayList<>(remainingLTS);
         nextRemaining.remove(subLTS);
-        recursiveAppend(subLTSTerminal, nextRemaining);
+        recursiveAppend(subLTSFinalState, nextRemaining);
       }
     }
   }
 
-  private void addLTSToTerminal(State terminal, LTS subLTS) {
+  /**
+   * Include the subLTS into lts but with finalState as start-state.
+   *
+   * @param finalState the new start-state of subLTS.
+   * @param subLTS     the lts that should be appended as outgoing of finalState in lts.
+   */
+  private void appendLTSToFinalState(State finalState, LTSWithFinalStates subLTS) {
     for (var startTransition : subLTS.getOutgoings(subLTS.getStart())) {
-      lts.addTransition(startTransition.changedSource(terminal));
+      lts.addTransition(startTransition.changedSource(finalState));
     }
     lts.addLTS(subLTS);
     // Remove start state and its outgoings.
     lts.removeState(subLTS.getStart());
   }
 
-  public static LTS subLTS(LTS lts, Transition startTransition) {
+  /**
+   * Create the sub-lts induced by the startTransition. We want startTransition to be in the sub-lts but no other
+   * transition outgoing from startTransition.getSource().
+   */
+  public static LTSWithFinalStates subLTS(LTSWithFinalStates lts, Transition startTransition) {
     var subLTSFromTarget = new LTSTraverser(lts).subLTS(startTransition.getTarget());
-    var subLTS = new LTS(startTransition.getSource());
+    // Create a copy of the subLTS-part.
+    var subLTS = new LTSWithFinalStates(startTransition.getSource());
+
     subLTS.addTransition(startTransition);
     subLTS.addTransitionsOf(subLTSFromTarget);
+    // Add all final states of lts that are present in subLTSFromTarget to subLTS.
+    subLTSFromTarget.getStates()
+        .stream()
+        .filter(state -> lts.getFinalStates().contains(state))
+        .forEach(subLTS::addAsFinalState);
     return subLTS;
-  }
-
-  /**
-   * Use depth-first-search to find all terminal states in oldLTS that are reachable from state.
-   *
-   * @return A list of all found terminal states.
-   */
-  public static List<State> reachableTerminalStates(LTS oldLTS, State state) {
-
-    List<State> foundTerminalStates = new ArrayList<>();
-    new LTSTraverser(oldLTS).depthFirstSearchLTS(
-        state,
-        (s) -> {
-          if (oldLTS.getOutgoings(s).isEmpty()) {
-            foundTerminalStates.add(s);
-          }
-        });
-    return foundTerminalStates;
   }
 
 }
--- bpmn-dsl/Workflow2SMT/src/test/java/de/monticore/bpmn/wf2lts/transformer/DefaultSequentialInterleavingTest.java
+++ bpmn-dsl/Workflow2SMT/src/test/java/de/monticore/bpmn/wf2lts/transformer/DefaultSequentialInterleavingTest.java
@@ -1,29 +1,35 @@
 package de.monticore.bpmn.wf2lts.transformer;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import de.monticore.bpmn.wf2lts.LTSTestingUtils;
-import de.monticore.bpmn.wf2lts.datastructure.LTS;
+import de.monticore.bpmn.wf2lts.Utils;
 import de.monticore.bpmn.wf2lts.datastructure.LTS.State;
 import de.monticore.bpmn.wf2lts.datastructure.LTS.Transition;
 import de.monticore.bpmn.wf2lts.datastructure.LTSTraverser;
+import de.monticore.bpmn.wf2lts.datastructure.LTSWithFinalStates;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DefaultSequentialInterleavingTest {
 
-  private static LTS buildSimpleLTS() {
-    var labels = List.of("A", "B", "C", "D");
-    var lts = new LTS();
+  private static LTSWithFinalStates buildSimpleLTS() {
+    var lts = new LTSWithFinalStates();
     LTSTestingUtils.addPathOfLabelFromStart(lts, List.of("A", "B"));
     LTSTestingUtils.addPathOfLabelFromStart(lts, List.of("C", "D"));
+    lts.getTerminalStates().forEach(lts::addAsFinalState);
     return lts;
   }
 
-  private static LTS buildThreePaths() {
+  private static LTSWithFinalStates buildThreePaths() {
     var lts = buildSimpleLTS();
     LTSTestingUtils.addPathOfLabelFromStart(lts, List.of("E", "F"));
+    lts.addAsFinalState(lts.getTransitionsForLabel("F").get(0).getTarget());
     return lts;
   }
 
@@ -39,22 +45,40 @@ class DefaultSequentialInterleavingTest {
     Assertions.assertFalse(LTSTraverser.pathOfLabel(interleaved, List.of("C", "A")).isPresent());
 
     LTSTestingUtils.assertSameStartOutgoingLabel(lts, interleaved);
+
+    var expectedFinalStates = Stream.concat(
+        interleaved.getTransitionsForLabel("B").stream().map(Transition::getTarget),
+        interleaved.getTransitionsForLabel("D").stream().map(Transition::getTarget)
+    ).collect(Collectors.toSet());
+    Assertions.assertEquals(expectedFinalStates, interleaved.getFinalStates());
+
   }
 
   @Test
   void testOnlyOnePath() {
-    var lts = new LTS();
+    var lts = new LTSWithFinalStates();
     LTSTestingUtils.addPathOfLabelFromStart(lts, List.of("A", "B"));
     lts.addTransition(
         new Transition(lts.getTransitionsForLabel("A").get(0).getTarget(), Collections.emptyList(), "C", new State()));
+    lts.getTerminalStates().forEach(lts::addAsFinalState);
 
     var interleaved = DefaultSequentialInterleaving.interleave(lts);
 
-    Assertions.assertEquals(1, interleaved.getOutgoings(interleaved.getStart()).size());
     LTSTestingUtils.assertSameStartOutgoingLabel(lts, interleaved);
     LTSTestingUtils.assertPathExists(interleaved, List.of("A", "B"));
     LTSTestingUtils.assertPathExists(interleaved, List.of("A", "C"));
     Assertions.assertEquals(2, interleaved.getTerminalStates().size());
+    Assertions.assertEquals(1, interleaved.getOutgoings(interleaved.getStart()).size());
+
+    var expectedFinalStates = Stream.of("B", "C")
+        .flatMap(label -> interleaved.getTransitionsForLabel(label).stream())
+        .map(Transition::getTarget)
+        .collect(Collectors.toSet());
+
+    Assertions.assertEquals(expectedFinalStates, interleaved.getFinalStates());
+    // Final states equal terminal states as there is only one path.
+    Utils.assertEqualIgnoreOrder(interleaved.getTerminalStates(), new ArrayList<>(interleaved.getFinalStates()));
+
   }
 
   @Test
@@ -74,6 +98,11 @@ class DefaultSequentialInterleavingTest {
             .collect(Collectors.toList());
 
     LTSTestingUtils.assertPathsExists(interleaved, threePathsLTSValidPaths);
+    var expectedFinalStates = Stream.of("B", "D", "F")
+        .flatMap(label -> interleaved.getTransitionsForLabel(label).stream())
+        .map(Transition::getTarget)
+        .collect(Collectors.toSet());
+    Assertions.assertEquals(expectedFinalStates, interleaved.getFinalStates());
   }
 
   @Test
@@ -83,8 +112,12 @@ class DefaultSequentialInterleavingTest {
     lts.addTransition( // Create cycle by transition from target of B, labeled Z, to source of B.
         new Transition(
             bTransition.getTarget(), Collections.emptyList(), "Z", bTransition.getSource()));
+    var bbTarget = new State();
     lts.addTransition(
-        new Transition(bTransition.getTarget(), Collections.emptyList(), "BB", new State()));
+        new Transition(bTransition.getTarget(), Collections.emptyList(), "BB", bbTarget));
+
+    lts.unmarkAsFinal(bTransition.getTarget()); // Unmark as final state
+    lts.addAsFinalState(bbTarget);
 
     var interleaved = DefaultSequentialInterleaving.interleave(lts);
 
@@ -103,6 +136,12 @@ class DefaultSequentialInterleavingTest {
     var pathToZ = LTSTraverser.pathOfLabel(interleaved, List.of("A", "B", "Z")).orElseThrow();
     // The target of the Z transition is the source of the B transition
     Assertions.assertEquals(pathToZ.getTransitions().get(1).getSource(), pathToZ.getTransitions().get(2).getTarget());
+
+    var expectedFinalStates = Stream.of("D", "BB")
+        .flatMap(label -> interleaved.getTransitionsForLabel(label).stream())
+        .map(Transition::getTarget)
+        .collect(Collectors.toSet());
+    Assertions.assertEquals(expectedFinalStates, interleaved.getFinalStates());
   }
 
   @Test
@@ -110,23 +149,22 @@ class DefaultSequentialInterleavingTest {
     var lts = buildThreePaths();
     // Add second successor to A transition
     var aTarget = lts.getTransitionsForLabel("A").get(0).getTarget();
-    lts.addTransition(new Transition(aTarget, Collections.emptyList(), "BB", new State()));
+    lts.addTransition(new Transition(aTarget, Collections.emptyList(), "End", new State()));
     // Create cycle D,Z
     var bTransition = lts.getTransitionsForLabel("D").get(0);
     lts.addTransition(
         new Transition(
             bTransition.getTarget(), Collections.emptyList(), "Z", bTransition.getSource()));
-    // Add new successor to D s.t. the C,D path ends in a terminal state
-    lts.addTransition(
-        new Transition(bTransition.getTarget(), Collections.emptyList(), "DD", new State()));
-
+    // The targets of "D", "B" and "F" are final states.
+    // Target of "End" is not a final state (but a terminal state)!
     var interleaved = DefaultSequentialInterleaving.interleave(lts);
+    var interleavedTraverser = new LTSTraverser(interleaved);
 
     LTSTestingUtils.assertSameStartOutgoingLabel(lts, interleaved);
 
     List<List<String>> threePathsLTSValidPaths =
         LTSTestingUtils.generatePermutations(
-                List.of(List.of("A", "B"), List.of("C", "D", "DD"), List.of("E", "F")))
+                List.of(List.of("A", "B"), List.of("C", "D"), List.of("E", "F")))
             .stream()
             .map(x -> x.stream().flatMap(List::stream).collect(Collectors.toList()))
             .collect(Collectors.toList());
@@ -135,19 +173,39 @@ class DefaultSequentialInterleavingTest {
       LTSTestingUtils.assertPathExists(interleaved, validPath);
     }
 
-    var backLinkPaths =
-        List.of(
-            List.of("A", "B", "C", "D", "Z", "D", "DD", "E", "F"),
-            List.of("C", "D", "Z", "D", "DD", "E", "F", "A", "B"),
-            List.of("C", "D", "DD", "E", "F", "A", "B"));
-    LTSTestingUtils.assertPathsExists(interleaved, backLinkPaths);
+    // Assert "End" after every "A" transition possible
+    var aWithoutEndOutgoing = interleaved.getTransitionsForLabel("A")
+        .stream()
+        .map(Transition::getTarget)
+        .map(interleavedTraverser::pathFrom)
+        .filter(path -> path.outgoingsWith("End").size() != 1)
+        .findAny();
+    assertTrue(aWithoutEndOutgoing.isEmpty());
 
-    var branchingAPaths =
-        List.of(
-            List.of("A", "B", "C", "D"),
-            List.of("A", "BB", "C"),
-            List.of("E", "F", "A", "BB", "C"),
-            List.of("E", "F", "A", "B", "C"));
-    LTSTestingUtils.assertPathsExists(interleaved, branchingAPaths);
+    // Assert no transition after "End"
+    var interleavedEndTargets = interleaved
+        .getTransitionsForLabel("End")
+        .stream()
+        .map(Transition::getTarget).collect(
+            Collectors.toList());
+    assertTrue(interleaved.getTerminalStates().containsAll(interleavedEndTargets));
+    assertTrue(interleavedEndTargets.stream().noneMatch(interleaved::isFinalState));
+
+    // Assert after every "D" transition a "Z" is possible.
+    var zOutgoingsFromB = interleaved.getTransitionsForLabel("D")
+        .stream()
+        .map(Transition::getTarget)
+        .map(interleavedTraverser::pathFrom)
+        .map(path -> path.outgoingsWith("Z"))
+        .collect(Collectors.toList());
+    assertTrue(zOutgoingsFromB.stream().allMatch(zPath -> zPath.size() == 1));
+
+    // Assert that every you have "D" followed by "Z" after every target of a "Z" transition
+    var noBZContinuation = zOutgoingsFromB
+        .stream()
+        .map(transitions -> transitions.get(0).getTarget())
+        .filter(zTarget -> interleavedTraverser.pathOfLabel(zTarget, List.of("D", "Z")).isEmpty())
+        .findAny();
+    assertTrue(noBZContinuation.isEmpty());
   }
 }
