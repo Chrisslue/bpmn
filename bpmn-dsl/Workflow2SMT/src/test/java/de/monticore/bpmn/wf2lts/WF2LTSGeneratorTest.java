e8c0a4be5f5681bf3fb578fb454dfc5738d2ec99
Max Philip Stachon <max.stachon@rwth-aachen.de>
2023-07-26 21:35:15 +0200

    minor fix

--- bpmn-dsl/Workflow2SMT/src/test/java/de/monticore/bpmn/wf2lts/WF2LTSGeneratorTest.java
+++ bpmn-dsl/Workflow2SMT/src/test/java/de/monticore/bpmn/wf2lts/WF2LTSGeneratorTest.java
@@ -1,10 +1,10 @@
 package de.monticore.bpmn.wf2lts;
 
+import de.monticore.bpmn.Resources;
 import de.monticore.bpmn.workflow.WorkflowMill;
 import de.monticore.lts.LTS2Mermaid;
 import de.monticore.symbols.basicsymbols.BasicSymbolsMill;
 import de.se_rwth.commons.logging.Log;
-import java.util.Objects;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,19 +20,16 @@ public class WF2LTSGeneratorTest {
     BasicSymbolsMill.initializePrimitives();
   }
 
-  private static String diagramFile = Objects.requireNonNull(WF2LTSGenerator.class.getResource("../Prototype.wfm"))
-      .getPath();
-
   @Test
   public void testLoading() {
-    WF2LTSGenerator.loadBPMN(diagramFile);
+    WF2LTSGenerator.loadBPMN(Resources.PROTOTYPE);
     Assertions.assertEquals(0, Log.getErrorCount());
   }
 
   @Test
   public void testComplete() {
 
-    var lts = WF2LTSGenerator.workflow2LTS(diagramFile);
+    var lts = WF2LTSGenerator.workflow2LTS(Resources.PROTOTYPE);
     System.out.println(lts.toModel(new LTS2Mermaid()).build());
     Assertions.assertEquals(1, lts.getOutgoings(lts.getStart()).size());
     Assertions.assertEquals("Start", lts.getOutgoings(lts.getStart()).get(0).getLabel());
