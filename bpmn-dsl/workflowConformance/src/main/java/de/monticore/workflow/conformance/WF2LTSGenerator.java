c951b2eba79063cd87cefadc3dcc6da40c77ecd3
Valdes Voufo <valdesvoufo2@gmail.com>
2024-07-23 14:11:15 +0200

    fmt

--- bpmn-dsl/workflowConformance/src/main/java/de/monticore/workflow/conformance/WF2LTSGenerator.java
+++ bpmn-dsl/workflowConformance/src/main/java/de/monticore/workflow/conformance/WF2LTSGenerator.java
@@ -6,16 +6,14 @@ import de.monticore.workflow.conformance.datastructure.BPMNNodeType;
 import de.monticore.workflow.conformance.utils.BPMNElementStorage;
 import de.monticore.workflow.conformance.utils.BPMNUtils;
 import de.se_rwth.commons.logging.Log;
-
 import java.util.*;
 
 public class WF2LTSGenerator {
 
   private BPMNElementStorage storage;
-  private  LTSBuilder ltsBuilder ;
+  private LTSBuilder ltsBuilder;
   private BPMNNode startEvent;
   private BPMNNode endNode;
-
 
   /***
    * transform a BPMN to a sequence of scope, as scope we have sequence, Xor, Loop ,Or, and And.
@@ -30,57 +28,48 @@ public class WF2LTSGenerator {
     sequence.add(ltsBuilder.mkEvent(startEvent.getName()));
 
     ASTFlowElement next = startEvent;
-    while (storage.hasNext(next)){
+    while (storage.hasNext(next)) {
       next = storage.getNext(next);
-         sequence.add(mkNode(next));
+      sequence.add(mkNode(next));
     }
 
     return ltsBuilder.mkSequence(sequence);
   }
 
-
-
   BPMNNode mkNode(ASTFlowElement node) {
 
-
-    if (node instanceof ASTTask){
-      return  ltsBuilder.mkTask(((ASTTask) node).getName());
-    } else if (node instanceof  ASTSequenceFlow) {
+    if (node instanceof ASTTask) {
+      return ltsBuilder.mkTask(((ASTTask) node).getName());
+    } else if (node instanceof ASTSequenceFlow) {
       List<BPMNNode> children = new ArrayList<>();
 
-      for (ASTFlowTarget tgt: ((ASTSequenceFlow) node).getPathList()){
+      for (ASTFlowTarget tgt : ((ASTSequenceFlow) node).getPathList()) {
         ASTFlowElement element = storage.getElement(tgt.getNodeRef().getBaseName());
       }
 
       return ltsBuilder.mkSequence(children);
 
-    }
-
-
-   else if (node instanceof  ASTNamedGateway){
+    } else if (node instanceof ASTNamedGateway) {
       List<BPMNNode> children = new ArrayList<>();
 
-   BPMNNodeType type = BPMNUtils.getGatewayType((ASTNamedGateway) node);
+      BPMNNodeType type = BPMNUtils.getGatewayType((ASTNamedGateway) node);
 
-    if (BPMNUtils.isSplit((ASTNamedGateway) node)){
-     ((ASTNamedGateway) node).forEachOutgoings( n-> children.add(mkNode((ASTFlowElement) n)));
-    }
+      if (BPMNUtils.isSplit((ASTNamedGateway) node)) {
+        ((ASTNamedGateway) node).forEachOutgoings(n -> children.add(mkNode((ASTFlowElement) n)));
+      }
 
-    switch (type){
-      case XOR: return ltsBuilder.mkXor(((ASTNamedGateway) node).getName(),children);
-        case OR:return  ltsBuilder.mkOr(((ASTNamedGateway) node).getName(),children);
-        case AND: return  ltsBuilder.mkAnd(((ASTNamedGateway) node).getName(),children);
+      switch (type) {
+        case XOR:
+          return ltsBuilder.mkXor(((ASTNamedGateway) node).getName(), children);
+        case OR:
+          return ltsBuilder.mkOr(((ASTNamedGateway) node).getName(), children);
+        case AND:
+          return ltsBuilder.mkAnd(((ASTNamedGateway) node).getName(), children);
         default:
-        Log.error("unsupported gateway type: "+type);
-        return  null ;
+          Log.error("unsupported gateway type: " + type);
+          return null;
+      }
     }
-    }
-
-   return  null ;
-
+    return null;
   }
-
-
-
-
 }
