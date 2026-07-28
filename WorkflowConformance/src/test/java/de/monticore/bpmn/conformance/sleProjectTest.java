cb5a3878e652b62e00591195cce64124211b8644
ChrissLue <christoph.luetticke0@gmail.com>
2026-07-28 10:32:52 +0200

    fucking finally

--- WorkflowConformance/src/test/java/de/monticore/bpmn/conformance/sleProjectTest.java
+++ WorkflowConformance/src/test/java/de/monticore/bpmn/conformance/sleProjectTest.java
@@ -1,41 +1,18 @@
 /*·(c)·https://github.com/MontiCore/monticore·*/
 package de.monticore.bpmn.conformance;
 
-import de.monticore.bpmn.cocos.WorkflowCoCos;
 import de.monticore.bpmn.workflow.WorkflowMill;
-import de.monticore.cd4analysis.CD4AnalysisMill;
-import de.monticore.cd4code.CD4CodeMill;
-import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
-import de.monticore.cdbasis._symboltable.CDBasisSymbols2Json;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class sleProjectTest extends AbstractConfTest {
-  
-  void loadCD(){
-    String folder = "target/cd2pojo/test/symbols/de/monticore/bpmn/conformance/sleProject/TimeManagement/";
-    
-    var symbolPaths = CD4AnalysisMill.globalScope().getSymbolPath();
-    symbolPaths.addEntry(Path.of(folder + "src/test/resources/"));
-    
-  }
-  
+
   @Test
-  void testTest() throws IOException {
+  void testTest() {
     init();
-    loadCD();
-    
-//    var parser = WorkflowMill.parser();
-//    var astroot = parser.parse("src/test/resources/de/monticore/bpmn/conformance/sleProject/TimeManagement.wfm");
-//    assertTrue(astroot.isPresent());
-//    assertFalse(parser.hasErrors());
-    var astroot2 = loadBPMN("de.monticore.bpmn.conformance.sleProject.TimeManagement", true);
-//    WorkflowMill.scopesGenitorDelegator().createFromAST(astroot.get());
-//    WorkflowCoCos.getBasicChecker().checkAll(astroot.get());
+    WorkflowMill.globalScope().getSymbolPath().addEntry(Path.of("target/cd2pojo/test/symbols/"));
+    var astroot = loadBPMN("de.monticore.bpmn.conformance.sleProject.TimeManagement", true);
   }
 }
