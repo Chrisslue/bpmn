1f1c4326d3b1d1202327be8e979e849cafa58e76
ChrissLue <christoph.luetticke0@gmail.com>
2026-07-28 10:51:28 +0200

    add explicit test

--- WorkflowConformance/src/test/java/de/monticore/bpmn/conformance/SleProjectTest.java
+++ WorkflowConformance/src/test/java/de/monticore/bpmn/conformance/SleProjectTest.java
@@ -2,17 +2,22 @@
 package de.monticore.bpmn.conformance;
 
 import de.monticore.bpmn.workflow.WorkflowMill;
+import de.se_rwth.commons.logging.Log;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
 
 
-public class sleProjectTest extends AbstractConfTest {
+public class SleProjectTest extends AbstractConfTest {
 
   @Test
   void testTest() {
     init();
     WorkflowMill.globalScope().getSymbolPath().addEntry(Path.of("target/cd2pojo/test/symbols/"));
-    var astroot = loadBPMN("de.monticore.bpmn.conformance.sleProject.TimeManagement", true);
+    
+    loadBPMN("de.monticore.bpmn.conformance.sleProject.TimeManagement", true);
+    
+    Assertions.assertTrue(Log.getFindings().isEmpty());
   }
 }
