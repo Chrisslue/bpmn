85de988c954de72e4cfe25f6b04881d8b7fba5e1
JoKurth <71062996+JoKurth@users.noreply.github.com>
2026-08-09 22:50:17 +0200

    fixed that the logger terminates the program; the negative test still throws to many errors and hence does not work correctly

--- WorkflowConformance/src/test/java/de/monticore/bpmn/conformance/SleProjectTest.java
+++ WorkflowConformance/src/test/java/de/monticore/bpmn/conformance/SleProjectTest.java
@@ -2,11 +2,13 @@
 package de.monticore.bpmn.conformance;
 
 import de.monticore.bpmn.workflow.WorkflowMill;
+import de.se_rwth.commons.logging.Finding;
 import de.se_rwth.commons.logging.Log;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
+import java.util.List;
 
 public class SleProjectTest extends AbstractConfTest {
   
@@ -24,11 +26,19 @@ public class SleProjectTest extends AbstractConfTest {
   @Test
   void failsForIncompleteSymbolsFromCD() {
     init();
-    WorkflowMill.globalScope().getSymbolPath().addEntry(Path.of("target/cd2pojo/test/symbols/de/monticore/bpmn/conformance/sleProject/TimeManagement_Incomplete"));
     
-    loadBPMN("de.monticore.bpmn.conformance.sleProject.TimeManagement_Incomplete", true);
+    // We have to start the Logger in interactive mode. Otherwise, it terminates the whole
+    // program if it has to log an error which negates the purpose of this test
+    Log.enableFailQuick(false);
+    
+    WorkflowMill.globalScope().getSymbolPath().addEntry(Path.of("target/cd2pojo/test/symbols/de/monticore/bpmn/conformance/sleProject/TimeManagementIncomplete"));
 
-    Assertions.assertFalse(Log.getFindings().isEmpty());
+    loadBPMN("de.monticore.bpmn.conformance.sleProject.TimeManagement", true);
+    
+    List<Finding> errors = Log.getFindings().stream().filter(Finding::isError).toList();
+
+    Assertions.assertEquals(1, errors.size());
+    Assertions.assertEquals("0xA0324 Cannot find symbol TimeSlotList", errors.getFirst().getMsg());
   }
   
 }
