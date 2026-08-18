bb2c003c2d79318d46ba605cf30900be47965e73
Peter Lindner <peter.lindner.sbg@gmail.com>
2026-08-18 18:09:03 +0200

    adjusted code formatting to fix failure of :WorkflowConformance:spotlessJavaCheck

--- WorkflowConformance/src/test/java/de/monticore/bpmn/conformance/SleProjectTest.java
+++ WorkflowConformance/src/test/java/de/monticore/bpmn/conformance/SleProjectTest.java
@@ -15,13 +15,13 @@ public class SleProjectTest extends AbstractConfTest {
   @Test
   void loadsSymbolsFromCD() {
     init();
-    WorkflowMill.globalScope().getSymbolPath().addEntry(Path.of("target/cd2pojo/test/symbols/de/monticore/bpmn/conformance/sleProject/TimeManagement"));
+    WorkflowMill.globalScope().getSymbolPath().addEntry(Path.of(
+        "target/cd2pojo/test/symbols/de/monticore/bpmn/conformance/sleProject/TimeManagement"));
     
     loadBPMN("de.monticore.bpmn.conformance.sleProject.TimeManagement", true);
     
     Assertions.assertTrue(Log.getFindings().isEmpty());
   }
-  
   
   @Test
   void failsForIncompleteSymbolsFromCD() {
@@ -31,12 +31,13 @@ public class SleProjectTest extends AbstractConfTest {
     // program if it has to log an error which negates the purpose of this test
     Log.enableFailQuick(false);
     
-    WorkflowMill.globalScope().getSymbolPath().addEntry(Path.of("target/cd2pojo/test/symbols/de/monticore/bpmn/conformance/sleProject/TimeManagementIncomplete"));
-
+    WorkflowMill.globalScope().getSymbolPath().addEntry(Path.of(
+        "target/cd2pojo/test/symbols/de/monticore/bpmn/conformance/sleProject/TimeManagementIncomplete"));
+    
     loadBPMN("de.monticore.bpmn.conformance.sleProject.TimeManagementIncomplete", true);
     
     List<Finding> errors = Log.getFindings().stream().filter(Finding::isError).toList();
-
+    
     Assertions.assertEquals(1, errors.size());
     Assertions.assertEquals("0xA0324 Cannot find symbol TimeSlotList", errors.getFirst().getMsg());
   }
